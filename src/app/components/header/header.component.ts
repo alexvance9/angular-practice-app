7659ab2c9bf542847932164f0104654ac3e31d30
alexvance9 <alexvance9@gmail.com>
2023-06-15 17:31:07 -0700

    toggle add button on about route

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UiService } from 'src/app/services/ui.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,15 @@ export class HeaderComponent {
   showAddTask: boolean = false;
   subscription!: Subscription;
 
-  constructor (private uiService: UiService) {
+  constructor (private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
   }
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) : boolean{
+    return this.router.url === route;
   }
 }
