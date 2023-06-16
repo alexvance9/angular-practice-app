edee229059726e62ecf6f748bf965ea56f6ac194
alexvance9 <alexvance9@gmail.com>
2023-06-15 17:28:06 -0700

    about page and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/pages/about/about.component';
+
 
 const routes: Routes = [
-  {path: '', component: TasksComponent}
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
